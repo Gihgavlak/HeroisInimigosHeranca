fdd9d5c1350537233619948f7476973fa8dee2df
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-09-23 14:13:40 -0300

    Adicionando Herança da classe Heroi

--- src/br/com/zup/Mago.java
+++ src/br/com/zup/Mago.java
@@ -1,10 +1,11 @@
 package br.com.zup;
 
-public class Mago {
+public class Mago extends Heroi{
     private double poderMagico;
 
-    public Mago(String nome, double poderMagico) {
-        this.poderMagico = poderMagico;
+    public Mago(String nome, double poderMagico, double poderMagico1) {
+        super(nome, poderMagico);
+        this.poderMagico = poderMagico1;
     }
 
     public double getPoderMagico() {
