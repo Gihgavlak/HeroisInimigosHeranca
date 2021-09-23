efb58fc19b384531d083a3f0d3ba484b34d8db42
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-09-23 14:04:07 -0300

    Estrutura da Classe InimigosVoadores

--- src/br/com/zup/InimigoVoadores.java
+++ src/br/com/zup/InimigoVoadores.java
@@ -1,2 +1,17 @@
-package br.com.zup;public class InimigoVoadores {
+package br.com.zup;
+
+public class InimigoVoadores {
+    private double danoTiro;
+
+    public InimigoVoadores(double danoTiro) {
+        this.danoTiro = danoTiro;
+    }
+
+    public double getDanoTiro() {
+        return danoTiro;
+    }
+
+    public void setDanoTiro(double danoTiro) {
+        this.danoTiro = danoTiro;
+    }
 }
