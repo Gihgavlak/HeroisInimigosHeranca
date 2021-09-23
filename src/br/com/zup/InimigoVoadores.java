85c972a739ce9cf6d389cb05158955b300e38e9b
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-09-23 14:19:43 -0300

    Adicionando Herança da Classe Inimigo

--- src/br/com/zup/InimigoVoadores.java
+++ src/br/com/zup/InimigoVoadores.java
@@ -1,9 +1,10 @@
 package br.com.zup;
 
-public class InimigoVoadores {
+public class InimigoVoadores extends Inimigo {
     private double danoTiro;
 
-    public InimigoVoadores(double danoTiro) {
+    public InimigoVoadores(String nome, double vida, double danoTiro) {
+        super(nome, vida);
         this.danoTiro = danoTiro;
     }
 
