fd194b199bb6fefc318f954db2872e6f28c4577c
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-09-23 13:47:14 -0300

    Estutura Classe Mago

--- src/br/com/zup/Mago.java
+++ src/br/com/zup/Mago.java
@@ -1,2 +1,27 @@
-package br.com.zup;public class Mago {
+package br.com.zup;
+
+public class Mago {
+    private String nome;
+    private double poderMagico;
+
+    public Mago(String nome, double poderMagico) {
+        this.nome = nome;
+        this.poderMagico = poderMagico;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPoderMagico() {
+        return poderMagico;
+    }
+
+    public void setPoderMagico(double poderMagico) {
+        this.poderMagico = poderMagico;
+    }
 }
