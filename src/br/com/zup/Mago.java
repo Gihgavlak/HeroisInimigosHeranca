80fc730f6d2b8952225ee1fc70603c897ab921d7
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-09-23 13:54:03 -0300

    Retirando Atributo nome pois consta na -mãe

--- src/br/com/zup/Mago.java
+++ src/br/com/zup/Mago.java
@@ -1,20 +1,10 @@
 package br.com.zup;
 
 public class Mago {
-    private String nome;
     private double poderMagico;
 
     public Mago(String nome, double poderMagico) {
-        this.nome = nome;
         this.poderMagico = poderMagico;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public double getPoderMagico() {
