e2428edb019993209523deab41828f6bdcafd87a
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-09-23 13:50:07 -0300

    Estrutura Classe Guerreiro

--- src/br/com/zup/Guerreiro.java
+++ src/br/com/zup/Guerreiro.java
@@ -1,2 +1,17 @@
-package br.com.zup;public class Guerreiro {
+package br.com.zup;
+
+public class Guerreiro {
+    private double poderAtaque;
+
+    public Guerreiro(double poderAtaque) {
+        this.poderAtaque = poderAtaque;
+    }
+
+    public double getPoderAtaque() {
+        return poderAtaque;
+    }
+
+    public void setPoderAtaque(double poderAtaque) {
+        this.poderAtaque = poderAtaque;
+    }
 }
