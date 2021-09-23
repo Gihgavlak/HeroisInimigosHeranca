82ef5ef884c7799bcfae18a011d4dfff9fea21f9
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-09-23 13:57:42 -0300

    Estrutura Classe InimigosRasteiros

--- src/br/com/zup/InimigoRasteiros.java
+++ src/br/com/zup/InimigoRasteiros.java
@@ -1,2 +1,17 @@
-package br.com.zup;public class InimigoRasteiros {
+package br.com.zup;
+
+public class InimigoRasteiros {
+    private double danosEspinhos;
+
+    public InimigoRasteiros(double danosEspinhos) {
+        this.danosEspinhos = danosEspinhos;
+    }
+
+    public double getDanosEspinhos() {
+        return danosEspinhos;
+    }
+
+    public void setDanosEspinhos(double danosEspinhos) {
+        this.danosEspinhos = danosEspinhos;
+    }
 }
